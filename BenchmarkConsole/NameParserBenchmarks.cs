90932a64048c8efeca23457d8380bbe2bdb46a37
danisylvester <maria.sylvester10@gmail.com>
2021-06-09 23:43:19 -0500

    Created an example benchmark.

---
+++ BenchmarkConsole/NameParserBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkConsole
+{
+    [MemoryDiagnoser]
+    public class NameParserBenchmark
+    {
+        private const string FullName = "Steve J Gordon";
+        private static readonly NameParser Parser = new NameParser();
+
+        [Benchmark(Baseline = true)]
+        public void GetLastName()
+        {
+            Parser.GetLastName(FullName);
+        }
+    }
+}
